9619b3037ee1f897bcfa6ace9aed865a1f564be8
Sindrefr <90753422+Sindrefr@users.noreply.github.com>
2023-06-06 10:29:02 +0200

    småfiks

--- app.js
+++ app.js
@@ -1,180 +1,161 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const app = express();
-require('dotenv').config()
+require('dotenv').config();
 const session = require('express-session');
-
+const sessionSecret = process.env.SECRET_KEY;
 
 app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
+// Konfigurerer Express session
 app.use(session({
-  secret: 'your-secret-key',
+  secret: sessionSecret,
   resave: false,
   saveUninitialized: false
 }));
 
-
-
-const uri = 'mongodb+srv://admin:eUOYaRaihHV7MwQA@cluster0.f0vgbuk.mongodb.net/quotes?retryWrites=true&w=majority';
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 let usersCollection;
 let quotesCollection;
 
-// Konfigurerer MongoDB Atlas connection string
+// Kobler til MongoDB-databasen
 client.connect().then(() => {
-    const database = client.db('quotes');
-    quotesCollection = database.collection('quotes');
-    usersCollection = database.collection('users');
-    console.log('Database connection successful');
-  }).catch(err => {
-    console.log('Error connecting to the database:', err);
-  });
+  const database = client.db('quotes');
+  quotesCollection = database.collection('quotes');
+  usersCollection = database.collection('users');
+  console.log('Database connection successful');
+}).catch(err => {
+  console.log('Error connecting to the database:', err);
+});
 
-
-  const requireAuth = (req, res, next) => {
+// Middleware for å sjekke om bruker er autentisert
+function requireAuth(req, res, next) {
+  // Sjekker om bruker er autentisert
   if (req.session.isLoggedIn) {
-    // User is authenticated, proceed to the next middleware
-    next();
+    // Sjekker om det forespurte brukernavnet samsvarer med det autentiserte brukernavnet
+    if (req.params.username === req.session.username) {
+      // Bruker er autentisert og får tilgang til siden
+      next();
+    } else {
+      // Hvis brukeren prøver å få tilgang til en annen brukers side
+      res.sendStatus(403); // Returnerer forbudt status
+    }
   } else {
-    // User is not authenticated, redirect to the login page
-    res.redirect('/login');
+    // Bruker er ikke autentisert
+    res.redirect('/login'); // Omdirigerer til innloggingssiden
   }
-};
+}
 
+// Middleware for å dele tilgang til isLoggedIn og username-variabler i alle views
 app.use((req, res, next) => {
   res.locals.isLoggedIn = req.session.isLoggedIn;
   res.locals.username = req.session.username;
   next();
 });
 
-
+// Registreringsrute
 app.get('/signup', (req, res) => {
-    res.render('signup', { feedback: '' });
-  });
-  
-  app.post('/signup', (req, res) => {
-    const { username, password, confirmPassword } = req.body;
-  
-    // Sjekk om brukeren allerede eksisterer i databasen
-    usersCollection.findOne({ username }).then(existingUser => {
-      if (existingUser) {
-        res.render('signup', { feedback: 'Brukernavn er allerede i bruk' });
-      } else if (password !== confirmPassword) {
-        res.render('signup', { feedback: 'Passordene samsvarer ikke' });
-      } else {
-        // Opprett brukeren i databasen
-        usersCollection.insertOne({ username, password }).then(() => {
-          res.render('signup', { feedback: 'Registrering fullført' });
-        }).catch(err => {
-          console.log('Error creating user:', err);
-          res.render('signup', { feedback: 'ERROR' });
-        });
-      }
-    })
-  });
+  res.render('signup', { feedback: '' });
+});
 
-  app.get('/login', (req, res) => {
-    res.render('login', { feedback: '' });
+app.post('/signup', (req, res) => {
+  const { username, password, confirmPassword } = req.body;
+
+  // Sjekker om brukeren allerede eksisterer i databasen
+  usersCollection.findOne({ username }).then(existingUser => {
+    if (existingUser) {
+      res.render('signup', { feedback: 'Brukernavn er allerede i bruk' });
+    } else if (password !== confirmPassword) {
+      res.render('signup', { feedback: 'Passordene samsvarer ikke' });
+    } else {
+      // Oppretter brukeren i databasen
+      usersCollection.insertOne({ username, password }).then(() => {
+        res.render('signup', { feedback: 'Registrering fullført' });
+      });
+    }
   });
-  
-  app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-  
-    // Check if the user exists in the database
-    usersCollection.findOne({ username }).then(user => {
-      if (user && user.password === password) {
-        // User authenticated, set session variables and redirect to their home page
-        req.session.isLoggedIn = true;
-        req.session.username = username;
-        res.redirect(`/home/${username}`);
-      } else {
-        res.render('login', { feedback: 'Invalid username or password' });
-      }
-    }).catch(err => {
-      console.log('Error checking user:', err);
-      res.render('login', { feedback: 'Error during login' });
-    });
+});
+
+// Innloggingsrute
+app.get('/login', (req, res) => {
+  res.render('login', { feedback: '' });
+});
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // Sjekker om brukeren eksisterer i databasen
+  usersCollection.findOne({ username }).then(user => {
+    if (user && user.password === password) {
+      // Bruker autentisert, setter sesjonsvariabler og omdirigerer til deres hjemmeside
+      req.session.isLoggedIn = true;
+      req.session.username = username;
+      res.redirect(`/home/${username}`);
+    } else {
+      res.render('login', { feedback: 'Ugyldig brukernavn eller passord' });
+    }
   });
-  
-  
-// Home page route
+});
+
+// Hjemmeside-rute
 app.get('/home/:username', requireAuth, (req, res) => {
   const username = req.params.username;
 
-  // Retrieve the quotes for the logged-in user
+  // Henter quotene for den innloggede brukeren
   quotesCollection.find({ author: username }).toArray()
     .then(quotes => {
       res.render('home', { username, quotes });
-    })
-    .catch(err => {
-      console.log('Error retrieving quotes:', err);
-      res.render('home', { username, quotes: [] });
     });
 });
 
-
-app.post('/addquote', requireAuth, (req, res) => {
+// Rute for å legge til en quote
+app.post('/addquote', (req, res) => {
   const username = req.session.username;
   const quote = req.body.quote;
 
-  // Create a new quote document
+  // Oppretter en ny quote-dokument
   const newQuote = {
     author: username,
     content: quote
   };
 
-  // Insert the new quote into the quotes collection
+  // Legger til den nye quoten i quotes-samlingen
   quotesCollection.insertOne(newQuote)
     .then(() => {
       res.redirect(`/home/${username}`);
-    })
-    .catch(err => {
-      console.log('Error adding quote:', err);
-      res.redirect(`/home/${username}`);
     });
 });
 
+// Rute for å vise quotene til en spesifikk forfatter
 app.get('/author/:author', (req, res) => {
   const author = req.params.author;
-  
-  // Retrieve quotes by the specified author from the quotes collection
+
+  // Henter quotene til den angitte forfatteren fra quotes-samlingen
   quotesCollection.find({ author }).toArray()
     .then(quotes => {
       res.render('author', { quotes, author });
-    })
-    .catch(err => {
-      console.log('Error retrieving quotes by author:', err);
-      res.redirect('/');
     });
 });
 
-
-
-
-
-// Logout route
+// Utlogging-rute
 app.get('/logout', (req, res) => {
-  // Clear the session and log out the user
+  // Tømmer sesjonen og logger ut brukeren
   req.session.destroy();
   res.redirect('/login');
 });
 
-
-
+// Hjemmeside-rute
 app.get('/', (req, res) => {
-  // Retrieve a random quote from the quotes collection
+  // Henter en tilfeldig quote fra quotes-samlingen
   quotesCollection.aggregate([{ $sample: { size: 1 } }]).toArray().then(quotes => {
     const quote = quotes[0];
-    res.render('index', { quote, isLoggedIn: req.session.isLoggedIn, username: req.session.username });
-  }).catch(err => {
-    console.log('Error retrieving quote:', err);
-    res.render('index', { quote: null, isLoggedIn: req.session.isLoggedIn, username: req.session.username });
+    res.render('index', { quote });
   });
 });
 
-
-
-  app.listen(3000, () => console.log('Listening on port 3000'));
+// Lytter på port 3000
+app.listen(3000, () => console.log('Lytter på port 3000'));
